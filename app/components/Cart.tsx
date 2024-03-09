2438d52442cc61b92a8e82c380f5616bb3712936
Milosz Olejnik <milosz.olejnik@tuta.io>
2024-03-09 19:40:52 +0100

    Create empty cart display

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -6,6 +6,7 @@ import formatPrice from '../util/priceFormat';
 import {IoAddCircle, IoRemoveCircle} from 'react-icons/io5'
 import { ProductType } from '../types/ProductType';
 import { AddToCartType } from '../types/AddToCartType';
+import basket from '../png/basket.png'
 
 export default function Cart(){
     const cartStore = useCartStore();
@@ -17,6 +18,8 @@ export default function Cart(){
             onClick={(e) => e.stopPropagation()} 
             className='
             bg-bgCard absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700'>
+                {cartStore.cart.length > 0 && (
+                    <>
                 <h1>here's your shopping list buddy 😎</h1>
                 {cartStore.cart && cartStore.cart.map((item) => (
                     <div className='flex py-4 gap-4'>
@@ -34,10 +37,18 @@ export default function Cart(){
                         </div>
                     </div>
                 ))}
-                <button 
-                className='py-2 mt-4 bg-accent  rounded-md text-white w-full'>
-                    Checkout
-                </button>
+                    <button 
+                    className='py-2 mt-4 bg-accent  rounded-md text-white w-full'>
+                        Checkout
+                    </button>
+                    </>
+                )}
+                {cartStore.cart.length <= 0 &&(
+                    <div className='flex flex-col items-center gap-12 text-2xl font-medium'>
+                        <h1>Oh, its empty innit? 😒</h1>
+                        <Image src={basket} alt="basket is empty" width={200} height={200} className='opacity-75'/>
+                    </div>
+                )}
             </div>
         </div>
     )
