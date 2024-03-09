11e0ff74875cfb2b38a47ccb59666c1ca78a87e7
Milosz Olejnik <milosz.olejnik@tuta.io>
2024-03-09 20:03:10 +0100

    add some comments and Total Price to cart

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -10,6 +10,11 @@ import basket from '../png/basket.png'
 
 export default function Cart(){
     const cartStore = useCartStore();
+    //Total Price
+    const totalPrice = cartStore.cart.reduce((acc, item) =>{
+        return acc + item.price! * item.quantity!
+    }, 0)
+
     return(
         <div 
         onClick={() =>{ cartStore.toggleCart()}} 
@@ -18,9 +23,11 @@ export default function Cart(){
             onClick={(e) => e.stopPropagation()} 
             className='
             bg-bgCard absolute right-0 top-0 w-1/4 h-screen p-12 overflow-y-scroll text-gray-700'>
+                {/* Check if there are any items in the cart to render */}
                 {cartStore.cart.length > 0 && (
                     <>
                 <h1>here's your shopping list buddy 😎</h1>
+                {/*List of items in the cart*/}
                 {cartStore.cart && cartStore.cart.map((item) => (
                     <div className='flex py-4 gap-4' key={item.id}>
                         <Image src={item.img as string} alt={item.name} width={120} height={120} className='rounded-md h-24' />
@@ -37,12 +44,15 @@ export default function Cart(){
                         </div>
                     </div>
                 ))}
+                {/* Checkout and Total Price */}
+                    <h1>Total: {formatPrice(totalPrice)}</h1>
                     <button 
                     className='py-2 mt-4 bg-accent  rounded-md text-white w-full'>
                         Checkout
                     </button>
                     </>
                 )}
+                {/* Empty cart message */}
                 {cartStore.cart.length <= 0 &&(
                     <div className='flex flex-col items-center gap-12 text-2xl font-medium'>
                         <h1>Oh, its empty innit? 😒</h1>
